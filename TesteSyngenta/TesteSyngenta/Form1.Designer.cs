85205c02fa224707f79a45e5280a17332df394c1
Wendrya Andrade <wendrya1@gmail.com>
2021-04-12 20:12:38 -0300

    Add files via upload

---
+++ TesteSyngenta/TesteSyngenta/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace TesteSyngenta
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(651, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Contar Pixels";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(167, 336);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 31);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Carregar imagem";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(36, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(403, 273);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(504, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 15);
+            this.label2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(979, 394);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Desafio Syngenta";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TesteSyngenta/TesteSyngenta/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TesteSyngenta
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var contaPixels = 0;
+            var contaPixelsPretos = 0;
+            var contaPixelsBrancos = 0;
+            var contaPixelsVerdes = 0;
+
+            Bitmap imagem = new Bitmap(pictureBox1.Image);
+
+            for (var x = 0; x < imagem.Width; x++)
+            {
+                for (var y = 0; y < imagem.Height; y++)
+                {
+                    contaPixels++;
+
+                    var corPixel = imagem.GetPixel(x, y).ToArgb();
+
+                    if (corPixel == Color.Black.ToArgb())
+                    {
+                        contaPixelsPretos++;
+                        //imagem.SetPixel(x, y, Color.White);
+                    }
+
+                    else if (corPixel == Color.White.ToArgb())
+                    {
+                        contaPixelsBrancos++;
+                        //imagem.SetPixel(x, y, Color.Black);
+                    }
+
+                    else
+                    {
+                        contaPixelsVerdes++;
+                        //imagem.SetPixel(x, y, Color.Red);
+                    }
+                }
+            }
+
+            pictureBox1.Image = imagem;
+
+            label2.Text = String.Format("A imagem contém " + contaPixels + " pixels, " + 
+                                        contaPixelsVerdes + " são verdes, " + 
+                                        contaPixelsPretos + " são pretos e " + 
+                                        contaPixelsBrancos + " são brancos");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "bmp|*.bmp";
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.ImageLocation = file.FileName;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
